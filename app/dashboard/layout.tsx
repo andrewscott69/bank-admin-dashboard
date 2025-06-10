b5b6c26a7b43fe2fade79dd6752adb802dbb9295
andrewscott69 <contactandrewscott69@gmail.com>
2025-06-10 16:46:54 -0700

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({
     <SidebarProvider>
       <div className="flex min-h-screen bg-background">
         <AdminSidebar />
-        <div className="flex-1 flex flex-col">
+        <div className="flex flex-col">
           <DashboardHeader />
           <main className="flex-1 p-6">{children}</main>
         </div>
